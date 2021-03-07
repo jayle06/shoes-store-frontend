031c6856a89fbc0ef5c385306a351accd941dc76
jayle06 <mhoang0611@gmail.com>
2021-03-07 15:41:11 +0700

    updata detail.css

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import data from '../data';
-import {useParams} from 'react-router-dom';
+import {Link,useParams} from 'react-router-dom';
 // import axios from 'axios';
+import '../css/Detail.css';
 
-function Detail() {
+function Detail(props) {
 
     // const[product, setProduct] = useState(null);
     let {id} = useParams();
     const product = data.products.find(product => product._id === id);
+    console.log(product);
     if(!product){
         return <div>404 Not Found!!!</div>;
     }    
@@ -15,11 +17,19 @@ function Detail() {
         <div className="detail">
             <img src={product.src} alt={product.title} />
             <div className="detail-content">
-                <h3>{product.title}</h3>
-                <span>${product.price}</span>
-                <p>{product.colors}</p>
-                <button>Add to cart</button>
+                <div className="detail-content-row">
+                    <h3>{product.title}</h3>
+                    <span>${product.price}</span>
+                </div>
+                <p>
+                    {product.colors.map((color, index) => (
+                        <button className="button-color" key={index} style={{ background:color}}></button>))}
+                </p>
+                <Link to='/cart' className="button-add-to-cart">
+                    Add to cart
+                </Link>
                 <p>{product.description}</p>
+                <p>{product.content}</p>
             </div>
         </div>
     )
